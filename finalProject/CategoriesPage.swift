93f57b065d75a01742c216625c4be08ae055a22f
Beatriz <beatrizdeferr@gmail.com>
2023-08-02 15:14:15 +0100

    added yet removed a lot of stuff

--- finalProject/CategoriesPage.swift
+++ finalProject/CategoriesPage.swift
@@ -10,50 +10,104 @@ import SwiftUI
 struct CategoriesPage: View {
     var body: some View {
         NavigationStack {
-                    
-                    ZStack {
-                        Color(red: 201/255.0, green: 215/255.0, blue: 248/255.0)
-                        Color(red: 201/255.0, green: 215/255.0, blue: 248/255.0)
-                            .ignoresSafeArea()
-                        RoundedRectangle(cornerRadius: 25)
-                            .fill(.white)
-                            .frame(width: 300, height: 300)
-                            .shadow(radius: 20)
-                        VStack {
-                            
-                            Text("Choose your first category!")
-                                .font(.title2)
-                            
-                                Spacer()
-                                .frame(height: 25)
-                            
-                                NavigationLink(destination: MovieGenresView()) {
-                                    Text("Movies")
-                                }
-                               
-                                
-                            Spacer()
-                                .frame(height: 25)
-                                                NavigationLink(destination: MovieGenresView()) {
-                                                    Text("Music")
-                                                }
-                            Spacer()
-                                                    .frame(height:25)
-                                                NavigationLink(destination: MovieGenresView()) {
-                                                    Text("Books")
-                                                }
-                            
-                          
-                            
-                        }
-                    }
-                }.navigationBarBackButtonHidden(true)
-            }
-        }
-      
             
-struct CategoriesPage_Previews: PreviewProvider {
-    static var previews: some View {
-        CategoriesPage()
+            ZStack {
+                Color(red: 201/255.0, green: 215/255.0, blue: 248/255.0)
+                    .ignoresSafeArea()
+                
+                
+               
+    
+                VStack {
+                    
+                    Text("Choose your first category!")
+                        .font(.title2)
+                        .frame(width: 500, height: 50)
+                        .background(Rectangle().fill(Color.white).shadow(radius: 3))
+                        .border(Color.gray)
+                      
+//
+                    Spacer()
+                        .frame(height: 200)
+//
+                    NavigationLink(destination: MovieGenresView()) {
+                        Text("Movies")
+
+                    }
+                    .frame(width: 100.0, height: 50.0)
+                    .border(/*@START_MENU_TOKEN@*/Color.purple/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/5/*@END_MENU_TOKEN@*/)
+                    .foregroundColor(/*@START_MENU_TOKEN@*/.white/*@END_MENU_TOKEN@*/)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.purple/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/50.0/*@END_MENU_TOKEN@*/)
+                    
+
+                    Spacer()
+                        .frame(height: 25)
+
+                    NavigationLink(destination: MovieGenresView()) {
+                        Text("Music")
+                    }
+                    Spacer()
+                        .frame(height:25)
+
+                    NavigationLink(destination: MovieGenresView()) {
+                        Text("Books")
+                        
+                    }
+                    
+                    
+                    Spacer()
+                        .frame(height:200)
+                }
+                
+            }
+            
+           
+            
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    HStack{
+                        NavigationLink(destination: RecsView()) {
+                            Image(systemName: "house")
+                        }
+                        .padding(2)
+                        //This takes you to the recommended list for movies
+                        NavigationLink(destination: RecsView()) {
+                            Image(systemName: "video")
+                        }
+                        .padding()
+                        //This takes you to the recommended list for music
+                        NavigationLink(destination: RecsView()) {
+                            Image(systemName: "headphones")
+                        }
+                        .padding()
+                        //This takes you to the recommended list for books
+                        NavigationLink(destination: RecsView()) {
+                            Image(systemName: "book")
+                        }
+                        .padding()
+                        //This takes you to the recommended list for list.bullet
+                        NavigationLink(destination: RecsView()) {
+                            Image(systemName: "list.bullet")
+                        }
+                        .padding(2)
+                    }
+                }
+                
+            }.navigationBarBackButtonHidden(true)
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.white)
+                .frame(width: 1000, height: 50)
+                .shadow(radius: 20)
+            
+            
+        }
+    }
+    
+    
+    struct CategoriesPage_Previews: PreviewProvider {
+        static var previews: some View {
+            CategoriesPage()
+        }
     }
 }
